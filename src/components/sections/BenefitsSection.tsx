bb58aa70f989441d6f61a7721e5f56ab266a50aa
mr-kushan <mr.kushand@gmail.com>
2025-07-17 10:58:37 +0530

    Update the styles of the benefits paragraphs

--- src/components/sections/BenefitsSection.tsx
+++ src/components/sections/BenefitsSection.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 const BenefitsSection: React.FC = () => (
   <section className="bg-white py-8 md:py-16 px-4 md:px-20">
     <div className="container mx-auto">
-      <h2>
+      <h2 className="text-4xl md:text-5xl text-gray-900 text-center mb-4">
 				<span>BENEFITS YOU HAVE ALWAYS</span>
 				<br />
-				<span>DREAMED OF</span>
+				<span className="font-extrabold text-purple-700">DREAMED OF</span>
 			</h2>
-			<p>
+			<p className="text-lg text-gray-700 text-center max-w-3xl mx-auto">
 				Every Time A Guest Pays With Qlub, You Earn Way More Than Just Revenue: More Tips, More Reviews, And More Data
 			</p>
 
